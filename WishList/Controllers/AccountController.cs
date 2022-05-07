b092c935f4013953fde0e4fc11cd5f67e8f1e567
Venkat Polur <venkat.polur@gmail.com>
2022-05-07 12:44:35 -0700

    adding a new controller

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        // GET: /<controller>/
+        public IActionResult Index()    
+        {
+            return View();
+        }
+    }
+}
