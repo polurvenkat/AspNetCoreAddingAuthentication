65c8c4d48803796ca4e1b036d9773f3544c12cc9
Venkat Polur <venkat.polur@gmail.com>
2022-05-07 12:46:51 -0700

    adding constructors

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -16,7 +16,11 @@ namespace WishList.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         // GET: /<controller>/
         public IActionResult Index()    
         {
