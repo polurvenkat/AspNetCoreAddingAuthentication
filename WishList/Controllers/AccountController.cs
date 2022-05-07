bf67328f526dca60d8faa7ab38668590c0db00d6
Venkat Polur <venkat.polur@gmail.com>
2022-05-07 13:46:23 -0700

    changes

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -84,6 +84,8 @@ namespace WishList.Controllers
             if (!result.Succeeded)
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+
+                return View(model);
             }
 
 
